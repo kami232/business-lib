4c8eeaa28cc61105824f41294938a27b57238186
kami232 <1347408207@qq.com>
2020-04-25 17:15:12 +0800

    add: 新增解析v-model和事件指令

--- mvvm2.0/compile.js
+++ mvvm2.0/compile.js
@@ -37,15 +37,18 @@ class Compile {
     return fragment;
   }
 
-  // 解析模板
+  // 解析模板， 解析指令也是在这里做
   compileElement(el) {
     let childNodes = el.childNodes;
     [].slice.call(childNodes).forEach(node => {
       const Reg = /\{\{(.*)\}\}/;
       const text = node.textContent;
 
-      // 判断是否是文本节点，并且是符合{{}}规则
-      if (this.isTextNode(node) && Reg.test(text)) {
+      if (this.isElementNode(node)) {
+        // 判断是否是元素节点
+        this.compileAttr(node);
+      } else if (this.isTextNode(node) && Reg.test(text)) {
+        // 判断是否是文本节点，并且是符合{{}}规则
         this.compileText(node, Reg.exec(text)[1].trim());
       }
 
@@ -54,6 +57,24 @@ class Compile {
         this.compileElement(node); // 继续遍历子节点
       }
     })
+  }
+
+  compileAttr(node) {
+    const nodeAttrs = node.attributes;
+    [].slice.call(nodeAttrs).forEach(attr => {
+      const attrName = attr.name;
+      // 判断是否是属性
+      if (this.isDirective(attrName)) {
+        const exp = attr.value; // 获取属性的值
+        const dir = attrName.substring(2); // 除掉前缀的指令名
+        if (this.isEventDirective(dir)) {
+          this.compileEvent(node, this.vm, exp, dir);
+        } else {
+          this.compileModel(node, this.vm, exp, dir);
+        }
+        node.removeAttribute(attrName); // 在节点上移除该属性
+      }
+    });
   }
 
   /**
@@ -69,9 +90,60 @@ class Compile {
     })
   }
 
+  /**
+   * 绑定事件
+   * @param node 节点
+   * @param vm 实例
+   * @param exp 指令属性值
+   * @param dir 指令名
+   */
+  compileEvent(node, vm, exp, dir) {
+    const eventType = dir.split(':')[1]; // 事件类型
+    const cb = vm.methods && vm.methods[exp];
+    if (eventType && cb) {
+      // 注意这里的cb上的this指向是指向被绑定的元素，我们需要把它改为实例，否则this是无法访问到实例上的data
+      node.addEventListener(eventType, cb.bind(vm), false);
+    }
+  }
+
+  /**
+   *
+   * @param node
+   * @param vm
+   * @param exp
+   * @param dir
+   */
+  compileModel(node, vm, exp, dir) {
+    let val = this.vm[exp]; // 获取该属性值的值
+    this.modelUpdater(node, val);
+    new Watcher(this.vm, exp, value => {
+      this.modelUpdater(node, value);
+    }); // 注册订阅者器
+
+    // 监听input事件，也是v-model的双向绑定原理
+    node.addEventListener('input', e => {
+      const newValue = e.target.value;
+      if (val === newValue) {
+        return;
+      }
+      this.vm[exp] = newValue;
+      val = newValue; // 缓存值，用于下次比较
+    });
+  }
+
   // 更新
   updateText(node, value) {
     node.textContent = typeof value == 'undefined' ? '' : value; // 更新{{}}的值
+  }
+
+  /**
+   * 修改input的value属性
+   * @param node 节点
+   * @param value 新值
+   * @param oldValue
+   */
+  modelUpdater(node, value, oldValue) {
+    node.value = typeof value == 'undefined' ? '' : value;
   }
 
   /**
@@ -81,6 +153,33 @@ class Compile {
    */
   isTextNode(node) {
     return node.nodeType == 3;
+  }
+
+  /**
+   * 判断是否是指令
+   * @param node
+   * @returns {boolean}
+   */
+  isDirective(attr) {
+    return attr.indexOf('v-') == 0;
+  }
+
+  /**
+   * 判断是否是事件指令
+   * @param dir
+   * @returns {boolean}
+   */
+  isEventDirective(dir) {
+    return dir.indexOf('on:') == 0;
+  }
+
+  /**
+   * 判断是否是元素节点
+   * @param node
+   * @returns {boolean}
+   */
+  isElementNode(node) {
+    return node.nodeType == 1;
   }
 }
 
--- mvvm2.0/index.js
+++ mvvm2.0/index.js
@@ -3,11 +3,14 @@ class SelfVue {
     this.vm = this;
     const ob = new Observe(opts.data);
     this.data = ob.data;
+    this.methods = opts.methods;
+
     // 此步骤是代理this.data，以至于无需this.data.xxx才可以访问到data的属性，可以直接this.xxx
     Object.keys(this.data).forEach(key => {
       this._proxyKeys(key);  // 绑定代理属性
     });
     new Compile(opts.el, this.vm);
+    opts.mounted && opts.mounted.call(this); // 所有事情处理好后执行mounted函数
     return this;
   }
 
