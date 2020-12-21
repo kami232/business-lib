37428fda5e0e1766b716f2e02fca838192f7544c
kim <1347408207@qq.com>
2020-12-21 22:17:09 +0800

    瀑布流

---
+++ func-components/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <WaterFall :hSpace=10 />
+</template>
+
+<script>
+import WaterFall from './components/WaterFall/index.vue'
+export default {
+  name: 'App',
+  components: {
+    WaterFall
+  }
+}
+</script>
